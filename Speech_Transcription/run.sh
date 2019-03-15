51c8bbf6e9f24a6cb1c006f7cf553b6297f62fae
amyjbaer <amyjbaer@umich.edu>
2019-03-15 14:57:34 -0400

    Fixed auth issues by export variable

--- Speech_Transcription/run.sh
+++ Speech_Transcription/run.sh
@@ -1,4 +1,6 @@
-if [ $# -eq 0 ] 
+export GOOGLE_APPLICATION_CREDENTIALS=$PWD/"key.json"
+
+if [ $# -eq 0 ]
 then
     echo "No arguments supplied"
     echo "Please provide relative path to .flac or .wav file you wish to analyze"
@@ -15,7 +17,7 @@ else
         echo "Please provide relative path to .wav or .flac file you wish to analyze"
         exit 1
     elif [ $code -eq 2 ]
-    then 
+    then
         filename="$(echo $filename | cut -d'.' -f 1).flac"
         echo "New File to analyze: $filename"
     fi
@@ -23,19 +25,24 @@ else
     # Runs through googleAPI
     API_OUT="$(python3 googleAPICall.py --input_file "$filename")"
 
-    if [ "$API_OUT" == "authentication needed" ]
-    then 
-        echo "Authentication needed"
-
-        gcloud config set project repeaterrepeater-1550350494511
-        gcloud projects add-iam-policy-binding repeaterrepeater-1550350494511 --member "serviceAccount:amyjbaer@repeaterrepeater-1550350494511.iam.gserviceaccount.com" --role "roles/owner"
-        gcloud iam service-accounts keys create RepeaterRepeater-90d8f74733ea.json --iam-account amyjbaer@repeaterrepeater-1550350494511.iam.gserviceaccount.com
-        
-        echo "Run this command:"
-        echo 'export GOOGLE_APPLICATION_CREDENTIALS=$PWD/"RepeaterRepeater-90d8f74733ea.json"'
-        echo "And run again"
-        exit 1
-    fi
+    echo $API_OUT
+#    if [ "$API_OUT" == "authentication needed" ]:
+#    then
+#        echo $API_OUT
+#        exit 1
+#    fi
+#    then
+#        echo "Authentication needed"
+#
+#        gcloud config set project repeaterrepeater-1550350494511
+#        gcloud projects add-iam-policy-binding repeaterrepeater-1550350494511 --member "serviceAccount:amyjbaer@repeaterrepeater-1550350494511.iam.gserviceaccount.com" --role "roles/owner"
+#        gcloud iam service-accounts keys create RepeaterRepeater-90d8f74733ea.json --iam-account amyjbaer@repeaterrepeater-1550350494511.iam.gserviceaccount.com
+#
+#        echo "Run this command:"
+#        echo 'export GOOGLE_APPLICATION_CREDENTIALS=$PWD/"RepeaterRepeater-90d8f74733ea.json"'
+#        echo "And run again"
+#        exit 1
+#    fi
 
     # Process google output
     PROCESSOR_OUT="$(python processor.py "$API_OUT")"

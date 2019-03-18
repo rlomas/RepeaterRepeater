d7138f7961fa3322c7184cc5a764e3d8765f6a70
amyjbaer <amyjbaer@umich.edu>
2019-03-18 11:30:43 -0400

    Added json extraction, pos scoring, punct scoring, and debug prints

--- Speech_Transcription/processorv2.py
+++ Speech_Transcription/processorv2.py
@@ -1,44 +1,72 @@
 import spacy
 import sys
+import json
+from collections import OrderedDict
+
 
 # (1) Pull out all strings and scores from JSON file
 
-    # if no score is given, bypass this step
+filename = sys.argv[1]
+with open(filename) as file:
+    data = json.load(file)
+
+google_results = OrderedDict()
+for result in data["results"][0]["alternatives"]:
+    confidence = -1
+    if "confidence" in result.keys():
+        confidence = result['confidence']
+    google_results[result["transcript"]] = confidence
 
 ##########################################
 
 
 
 # (2) Get score from POS analytics
-input = sys.argv[1].decode('utf-8')
-nlp = spacy.load('en_core_web_sm')
+def pos_analyze(transcript):
+    nlp = spacy.load('en_core_web_sm')
+    doc = nlp(transcript, 'utf-8')
 
-doc = nlp(input)
+    nouns = 0
+    verbs = 0
+    subject = 0
 
-nouns = 0
-verbs = 0
-subject = 0
+    # identifies parts of speech of each word
+    for token in doc:
+        if token.pos_ == u"VERB":
+            verbs += 1
+        if token.pos_ == u"NOUN":
+            nouns += 1
+        if 'subj' in token.dep_:
+            subject += 1
 
-# identifies parts of speech of each word
-for token in doc:
-    if token.pos_ == u"VERB":
-        verbs += 1
-    if token.pos_ == u"NOUN":
-        nouns += 1
-    if 'subj' in token.dep_:
-        subject += 1
+    # TODO: come up with scoring system that's better
+    score = 0
+    if verbs > 0:
+        score += 1
+    if nouns > 0:
+        score += 1
+    if subject > 0:
+        score += 1
 
-print("Nouns " + str(nouns))
-print("Verbs " + str(verbs))
-print("Subjects " + str(subject))
+    return score
+
+pos_scores = OrderedDict()
+for transcript in google_results.keys():
+    pos_scores[transcript] = pos_analyze(transcript)
+
 ##########################################
 
 
 
 # (3) Look at punctutation from google string
+punctutation_scores = OrderedDict()
+for transcript in google_results.keys():
+    # TODO: Maybe add more points for questions, periods?
+    punctutation_scores[transcript] = (transcript[-1] == '!' or transcript[-1] == '.' or transcript[-1] == '?')
 
 ##########################################
 
+# TODO: should factor in the ordering of the google results (hence the orderedDict)
 
 
 # (3) Cacluate final score and choose best option
@@ -48,6 +76,14 @@ print("Subjects " + str(subject))
 
 
 # (4) If debug flag, print out why option was chosen
+if len(sys.argv) > 2 and (sys.argv[2] == '-d' or sys.argv[2] == '--debug'):
+    for transcript in google_results.keys():
+        print(transcript)
+        print("\tTotal Score: ", "TODO")
+        print("\tGoogle Confidence :", google_results[transcript])
+        print("\tPOS Score: ", pos_scores[transcript])
+        print("\tPunctuation Score: ", punctutation_scores[transcript])
+        print("\tOrdering Adjustment: ", "TODO")
 
 ##########################################
 

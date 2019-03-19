febfbd1415cb12e8898313e131b4e20b7c37c977
Jason Kuker <jkuker@umich.edu>
2019-03-19 00:53:55 -0400

    record on button press is working

--- record.py
+++ record.py
@@ -1,45 +1,81 @@
-# code to get the device index (on my MAC, MIC is index 0)
-# import pyaudio
-# p = pyaudio.PyAudio()
-# for ii in range(p.get_device_count()):
-# 	print(p.get_device_info_by_index(ii).get('name'))
-
 import pyaudio
 import wave
+import time
+from squid import *
+from button import *
 
+
+# globals
 form_1 = pyaudio.paInt16 # 16-bit resolution
 chans = 1 # 1 channel
 samp_rate = 44100 # 44.1kHz sampling rate
 chunk = 4096 # 2^12 samples for buffer
-record_secs = 5 # seconds to record
-dev_index = 0 # device index found by p.get_device_info_by_index(ii)
+record_secs = 10 # seconds to record
+dev_index = 2 # device index found by p.get_device_info_by_index(ii)
 wav_output_filename = 'test1.wav' # name of .wav file
 
-audio = pyaudio.PyAudio() # create pyaudio instantiation
 
-# create pyaudio stream
-stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
-                    input_device_index = dev_index,input = True, \
-                    frames_per_buffer=chunk)
-print("recording")
-frames = []
+class Record(object):
+    
+    def __init__(self):
+        self.audio = pyaudio.PyAudio()
+        self.frames = []
+        self.stream = None
+        self.light = Squid(18,23,24)
+        self.light.set_color(GREEN)
+        self.button = Button(25)
+        self.time_elapsed = None
 
-# loop through stream and append audio chunks to frame array
-for _ in range(0,int((samp_rate/chunk)*record_secs)):
-    data = stream.read(chunk)
-    frames.append(data)
+    def run_loop(self):
+        while True:
+            if self.button.is_pressed():
+                # starts recording
+                print("recording begins")
+                self.time_elapsed = time.time()
+                self.light.set_color(RED)
+                self.record()
 
-print("finished recording")
+    def open_stream(self):
+        self.stream = self.audio.open(format=form_1,
+                                      rate=44100,
+                                      channels=chans,
+                                      input_device_index=2,
+                                      input=True,
+                                      frames_per_buffer=chunk)
 
-# stop the stream, close it, and terminate the pyaudio instantiation
-stream.stop_stream()
-stream.close()
-audio.terminate()
+    def record(self):
 
-# save the audio frames as .wav file
-wavefile = wave.open(wav_output_filename,'wb')
-wavefile.setnchannels(chans)
-wavefile.setsampwidth(audio.get_sample_size(form_1))
-wavefile.setframerate(samp_rate)
-wavefile.writeframes(b''.join(frames))
-wavefile.close()
+        self.open_stream()
+
+        while True:
+            if self.button.is_pressed():
+                self.time_elapsed = time.time() - self.time_elapsed
+                print("recording time: " + str(self.time_elapsed))
+                print("end recording")
+                self.save()
+                self.light.set_color(GREEN)
+                break
+
+            data = self.stream.read(chunk, exception_on_overflow=False)
+            self.frames.append(data)
+
+    def save(self):
+        # stop the stream, close it, and terminate the pyaudio instantiation
+        self.stream.stop_stream()
+        self.stream.close()
+
+        # save the audio frames as .wav file
+        wavefile = wave.open(wav_output_filename,'wb')
+        wavefile.setnchannels(chans)
+        wavefile.setsampwidth(self.audio.get_sample_size(form_1))
+        wavefile.setframerate(samp_rate)
+        wavefile.writeframes(b''.join(self.frames))
+        wavefile.close()
+        print("saved file ")
+
+
+if __name__ == "__main__":
+    rec = Record()
+    rec.run_loop()
+    rec.audio.terminate()
+

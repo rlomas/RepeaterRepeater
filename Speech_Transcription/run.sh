5aa4aa2bc2b4869160789e227be431204758c13b
amyjbaer <amyjbaer@umich.edu>
2019-03-19 12:58:05 -0400

    cleaned up run.sh

--- Speech_Transcription/run.sh
+++ Speech_Transcription/run.sh
@@ -1,55 +1,21 @@
+# for google auth : WILL ONLY WORK ON RASPBERRY PI
 export GOOGLE_APPLICATION_CREDENTIALS=$PWD/"raspberry-pi-key.json"
 
-# if [ $# -eq 0 ]
-# then
-#    echo "No arguments supplied"
-#    echo "Please provide relative path to .flac or .wav file you wish to analyze"
-# else
-    # echo "File to analyze: $1"
-    python3 ../record.py
-    python3 fileConvert.py "test1.wav"
-    filename="test1.flac"
-    # code=$?
-    # filename=$1
-    # echo $filename
-    # if [ $code -eq 1 ]
-    # then
-    #    echo "Invalid file extension"
-    #    echo "Please provide relative path to .wav or .flac file you wish to analyze"
-    #    exit 1
-    #elif [ $code -eq 2 ]
-    #then
-    #    filename="$(echo $filename | cut -d'.' -f 1).flac"
-    #    echo "New File to analyze: $filename"
-    #fi
+# Record on Raspberry Pi
+python3 ../record.py
 
-    # Runs through googleAPI
-    API_OUT="$(python3 googleAPICall.py --input_file "$filename")"
+# Convert file to flace
+python3 fileConvert.py "test1.wav"
 
-    echo $API_OUT
-#    if [ "$API_OUT" == "authentication needed" ]:
-#    then
-#        echo $API_OUT
-#        exit 1
-#    fi
-#    then
-#        echo "Authentication needed"
-#
-#        gcloud config set project repeaterrepeater-1550350494511
-#        gcloud projects add-iam-policy-binding repeaterrepeater-1550350494511 --member "serviceAccount:amyjbaer@repeaterrepeater-1550350494511.iam.gserviceaccount.com" --role "roles/owner"
-#        gcloud iam service-accounts keys create RepeaterRepeater-90d8f74733ea.json --iam-account amyjbaer@repeaterrepeater-1550350494511.iam.gserviceaccount.com
-#
-#        echo "Run this command:"
-#        echo 'export GOOGLE_APPLICATION_CREDENTIALS=$PWD/"RepeaterRepeater-90d8f74733ea.json"'
-#        echo "And run again"
-#        exit 1
-#    fi
+# Runs through googleAPI
+API_OUT="$(python3 googleAPICall.py --input_file "test1.flac")"
 
-    # Process google output
-    PROCESSOR_OUT="$(python processor.py "$API_OUT")"
+echo $API_OUT
 
-    echo "Text Found: " $PROCESSOR_OUT
+# Process google output
+PROCESSOR_OUT="$(python processor.py "$API_OUT")"
 
-    # Speak the processed output
-    ./textToSpeech.sh "$PROCESSOR_OUT"
-# fi
+echo "Text Found: " $PROCESSOR_OUT
+
+# Speak the processed output
+./textToSpeech.sh "$PROCESSOR_OUT"

9d16da26ae33a3d33047d9dfeec5539907adc998
Amy Baer <amyjbaer@umich.edu>
2019-04-15 18:26:11 -0400

    Changed error message

--- Speech_Transcription/processorv2.py
+++ Speech_Transcription/processorv2.py
@@ -15,7 +15,7 @@ if "error" in data.keys():
     exit(1)
 
 if "results" not in data.keys():
-    print("No Response from Google, please try again")
+    print("No Response from Google, please speak loudy and try again")
     exit(1)
 
 google_results = OrderedDict()
